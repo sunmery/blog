24ced717f29de4d899eddc37420a53d3369f0b5c
mandala <rcc@vip.qq.com>
2024-05-16 17:20:54 +0800

    chore: change domain

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,8 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://lookeke.com',
+  // url: 'https://lookeke.com',
+  url: 'https://lookeke.cn',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
